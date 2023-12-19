49f09552893c5d894e5c7ca7b66b975d0a745301
Abdallah Elagamy <112352249+AbdallahElagamy@users.noreply.github.com>
2023-12-19 23:03:40 +0200

    Update FCFS

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 #include <tuple>
-#include <cmath>
-#include <iomanip>
-#include <windows.h>
+#include <set>
 using namespace std;
 
 class Process
 {
 private:
-    int waitingTime, arrivalTime, burstTime, priority, executionTime;
+    int waitingTime, arrivalTime, burstTime,responseTime,turnAroundTime,id;
 
 public:
-    Process() : waitingTime{}, arrivalTime{}, burstTime{}, priority{}, executionTime{}
+    Process() : waitingTime{}, arrivalTime{}, burstTime{},responseTime{}
     {
     }
-    void set(int arrival = 0, int burst = 0, int p = 0)
-    {
-        arrivalTime = arrival;
-        burstTime = burst;
-        priority = p;
+    void set_arrival(int a){
+        arrivalTime=a;
     }
-    void set_executiontime(int execution)
+    void set_burst(int b)
     {
-        executionTime = execution;
+        burstTime = b;
+    }
+    void set_id(int i)
+    {
+        id = i;
     }
     int get_arrival()
     {
@@ -35,168 +33,94 @@ public:
     {
         return burstTime;
     }
-    int get_priority()
-    {
-        return priority;
-    }
-    int get_execution()
-    {
-        return executionTime;
+    int get_id(){
+        return id;
     }
     int get_waiting()
     {
         return waitingTime;
     }
-    void calcWaitingtime()
+    int get_response(){
+        return responseTime;
+    }
+    int get_Turnaround(){
+        return turnAroundTime;
+    }
+    void calcWaitingtime(int currentTime)
     {
-        // Calculate the over all time the process has taken in the waiting queue
-        waitingTime = executionTime - arrivalTime;
+        // Calculate the over all-time the process has taken in the waiting queue
+        waitingTime = currentTime - arrivalTime;
+        responseTime = waitingTime;
+    }
+    void calcTurnAroundTime() {
+        turnAroundTime = waitingTime + burstTime;
     }
 };
 
-void detectPriorityErrors(Process p[], int len)
-{
-    for (int i = 0; i < len; i++)
-    {
-        int currentPriority = p[i].get_priority();
-        if (currentPriority < 0)
-        {
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-            cout << "Invalid Input, the priority of process" << i + 1 << " is smaller than 0\n";
-            SetConsoleTextAttribute(hConsole, BACKGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-            cout << "Hint: Check Note Number 1";
-            SetConsoleTextAttribute(hConsole, 7);
-            exit(0);
-        }
-    }
-}
-
-// Mapping the process's times and type of errors
-string index[3] = {"Arrival Time", "Burst Time", "Priority"};
-
-void detectTimeErrors(int x, int i)
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-    if (fmod(x, 1.0) != 0 && x < 0)
-    {
-        cout << "Invalid Input, " << index[i] << " has to be an integer and positive number";
-        SetConsoleTextAttribute(hConsole, 7);
-        exit(0);
-    }
-    else if (fmod(x, 1.0) != 0)
-    {
-        cout << "Invalid Input, " << index[i] << " has to be an integer number";
-        SetConsoleTextAttribute(hConsole, 7);
-        exit(0);
-    }
-    else if (x < 0)
-    {
-        cout << "Invalid Input, " << index[i] << " has to be a positive number";
-        SetConsoleTextAttribute(hConsole, 7);
-        exit(0);
-    }
-    SetConsoleTextAttribute(hConsole, 7);
-}
-
 struct FCFS
 {
-    bool operator()(tuple<pair<int, int>, int, int> x, tuple<pair<int, int>, int, int> y)
-    {
-        if (get<0>(x).second > get<0>(y).second)
-            return true;
-        else if (get<0>(x).second == get<0>(y).second && get<2>(x) > get<2>(y))
-            return true;
-        else if (get<0>(x).second == get<0>(y).second && get<2>(x) == 0 && get<1>(x) > get<1>(y))
-            return true;
-        return false;
-    }
-    void process(Process p[], int len)
-    {
-        priority_queue<tuple<pair<int, int>, int, int>, vector<tuple<pair<int, int>, int, int>>, FCFS> readyQueue;
-        for (int i = 0; i < len; i++)
-        {
-            int currentArrival = p[i].get_arrival();
-            int currentBurst = p[i].get_burst();
-            int currentPriority = p[i].get_priority();
-            tuple<pair<int, int>, int, int> currentProcess({i, currentArrival}, currentBurst, currentPriority);
-            readyQueue.push(currentProcess);
-        }
-        int currentExecutionTime = -1;
-        cout << "\n\nThe Order of execution: \n";
-        while (readyQueue.size())
-        {
-            tuple<pair<int, int>, int, int> c = readyQueue.top();
-            int processNumber = get<0>(c).first;
-            int arrivalTime = get<0>(c).second;
-            int burstTime = get<1>(c);
-            cout << "\tProcess: " << processNumber + 1 << '\n';
-            if (currentExecutionTime < arrivalTime)
-            {
-                currentExecutionTime = arrivalTime;
+    multiset<tuple<int,int,int>>p,readyQueue;
+    vector<Process>original;
+    FCFS(vector<Process> a) {
+        original = a;
+        for (int i = 1; i < a.size(); i++)
+            p.insert({a[i].get_arrival(), a[i].get_burst(), a[i].get_id()});
+        int currentTime = get<0>(*p.begin());
+        vector<int> GantChart;
+        while (true) {
+            while (!p.empty() && currentTime >= get<0>(*p.begin())) {
+                auto current = *p.begin();
+                auto target = make_tuple(get<1>(current), get<0>(current), get<2>(current));
+                readyQueue.insert(target);
+                p.erase(p.begin());
             }
-            p[processNumber].set_executiontime(currentExecutionTime);
-            p[processNumber].calcWaitingtime();
-            currentExecutionTime += burstTime;
-            readyQueue.pop();
+            auto currentProcess = *readyQueue.begin();
+            int id = get<2>(currentProcess), burstTime = get<0>(currentProcess);
+            GantChart.push_back(id);
+            original[id].calcWaitingtime(currentTime);
+            original[id].calcTurnAroundTime();
+            readyQueue.erase(readyQueue.begin());
+            currentTime += burstTime;
+            if (readyQueue.empty() && !p.empty())
+                currentTime = max(currentTime, get<0>(*p.begin()));
+            if (readyQueue.empty() && p.empty())
+                break;
         }
-        cout << "\nWaiting Time: \n";
-        double avg = 0;
-        for (int i = 0; i < len; i++)
-        {
-            cout << "\tProcess " << i + 1 << ": " << p[i].get_waiting() << '\n';
-            avg += p[i].get_waiting();
+        cout << "\nGantChart\n";
+        int n = original.size() - 1, avg;
+        for (int i = 0; i < n; i++)
+            cout << "p" << GantChart[i] << " ";
+        cout << "\n\nWaitingTime\n";
+        for (int i = 1; i <= n; i++) {
+            cout << "p" << i << ": " << original[i].get_waiting() << '\n';
+            avg += original[i].get_waiting();
         }
-        cout << "\nAverage Waiting Time: \n";
-        cout << fixed << setprecision(2) << (avg / len * 1.0);
+        cout << "AverageWaitingTime " << avg / n;
+        cout << "\n\nResponseTime\n";
+        for (int i = 1; i <= n; i++)
+            cout << "p" << i << ": " << original[i].get_response() << '\n';
+        cout<<"\n\nTurnAroundTime\n";
+        for(int i=1;i<=n;i++)
+            cout << "p" << i << ": " << original[i].get_Turnaround() << '\n';
     }
 };
 
 int main()
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, BACKGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-    cout << "Notes and Comments: \n";
-    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
-    cout << "\t 1- The value of process's priority has to be a positive numbers.\n";
-    cout << "\t 2- The value of process's arrivaltime and bursttime has to be integer and positive number\n";
-    cout << "\t 3- If you put a value of any process's priority, then you've to put a value for the remaining.\n";
-    cout << "\t 4- If there is no priority and there is multiple tasks with the same arrival time, then the minimmum in burst time will be taken first.\n";
-    cout << "\t 5- If there is no priority and there is multiple tasks with the same arrival time and the same burst time, then the first in position will be taken first.\n";
-
-    SetConsoleTextAttribute(hConsole, 7);
-    cout << "\n\nNumber of Processes: ";
     int numberOfProcesses;
     cin >> numberOfProcesses;
-    Process p[numberOfProcesses];
-
-    cout << '\n';
+    vector<Process> p(numberOfProcesses+1);
     for (int i = 0; i < numberOfProcesses; i++)
     {
-        double x, y, z = 0;
-        string stateOfPriority;
-        cout << "\nProcess " << i + 1 << ": \n";
-        cout << "\tArrival Time: ";
-        cin >> x;
-        detectTimeErrors(x, 0);
-        cout << "\tBurst Time: ";
-        cin >> y;
-        detectTimeErrors(y, 1);
-        cout << "\tIs there any priority [Yes,No]: ";
-        cin >> stateOfPriority;
-        transform(stateOfPriority.begin(), stateOfPriority.end(), stateOfPriority.begin(), ::tolower);
-        if (stateOfPriority == "yes")
-        {
-            cout << "\t\tPriority: ";
-            cin >> z;
-            detectTimeErrors(z, 2);
-        }
-        p[i].set((int)x, (int)y, (int)z);
+        string name;
+        int bt,at;
+        cin>>name>>bt>>at;
+        int id = name[1]-'0';
+        p[id].set_arrival(at);
+        p[id].set_burst(bt);
+        p[id].set_id(id);
     }
-    detectPriorityErrors(p, numberOfProcesses);
-    FCFS calc;
-    calc.process(p, numberOfProcesses);
+    FCFS calc(p);
 
     return 0;
 }
